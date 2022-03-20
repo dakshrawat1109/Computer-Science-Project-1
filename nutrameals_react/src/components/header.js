89c7a9cb6ee5adb4c3443d75479f6fe073c7d762
Sanket-rgb <67689067+Sanket-rgb@users.noreply.github.com>
2022-03-20 13:17:38 -0400

    Changes to homepage Search UI

--- nutrameals_react/src/components/header.js
+++ nutrameals_react/src/components/header.js
@@ -1,31 +1,50 @@
-import React, {Component} from "react";
-import Logo from '../assets/img/NutraMeals-removebg-preview.png';
+import React, { Component } from "react";
+import Logo from "../assets/img/NutraMeals-removebg-preview.png";
+import "../App.css";
 
 class Header extends Component {
-	render() {
-		return (
-			<header id="header" class="fixed-top d-flex align-items-cente">
-                    <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
-                        <a href="index.html"><img src={Logo} alt=""></img></a>
-                        <h1 class="logo me-auto me-lg-0"></h1>
-                        {/*Uncomment below if you prefer to use an image logo
+  render() {
+    return (
+      <header id="header" class="fixed-top d-flex align-items-center">
+        <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
+          {/* <a href="index.html">
+            <img src={Logo} alt="" style={logo}></img>
+          </a> */}
+          <h1 class="logo me-auto me-lg-0"></h1>
+          {/*Uncomment below if you prefer to use an image logo
                         <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>*/}
 
-                        <nav id="navbar" class="navbar order-last order-lg-0">
-                            <ul>
-                            <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-                            <li><a class="nav-link scrollto" href="#about">About</a></li>
-                            <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-                            </ul>
-                            <i class="bi bi-list mobile-nav-toggle"></i>
-                        </nav>
-                        <a href="#book-a-table" class="book-a-table-btn scrollto d-none d-lg-flex">Login</a>
-                    </div>
-            </header>
-  //End Header 
-  
-		);
-	}
+          <nav id="navbar" class="navbar order-last order-lg-0">
+            <ul>
+              <li>
+                <a class="nav-link scrollto active" href="#hero">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a class="nav-link scrollto" href="#about">
+                  About
+                </a>
+              </li>
+              <li>
+                <a class="nav-link scrollto" href="#contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+          </nav>
+          <a
+            href="#book-a-table"
+            class="book-a-table-btn scrollto d-none d-lg-flex"
+          >
+            Login
+          </a>
+        </div>
+      </header>
+      //End Header
+    );
+  }
 }
 
 export default Header;
--- nutrameals_react/src/components/home.js
+++ nutrameals_react/src/components/home.js
@@ -1,27 +1,70 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import "../App.css";
 
-class Home extends Component{
-    render(){
-        return(
-            <section id="hero" class="d-flex align-items-center">
-            <div class="container position-relative text-center text-lg-start" data-aos="zoom-in" data-aos-delay="100">
-              <div class="row">
-                <div class="col-lg-8">
-                  <h1>NutraMeals</h1>
-                  <h2>Your Healthy Food Delivery Partner</h2>
-                  <input role="combobox" id="mySearch" onkeyup="myFunction()" placeholder="Search.." title="Type in a category"></input>
-                  <div class="btns">
-                    <a href="#menu" class="btn-menu animated fadeInUp scrollto">Menu</a>
-                    <a href="#book-a-table" class="btn-book animated fadeInUp scrollto">Restaurants</a>
-                  </div>
-                </div>
-        
-              </div>
+class Home extends Component {
+  render() {
+    return (
+      <section id="hero">
+        <div
+          style={{
+            position: "absolute",
+            left: "60%",
+            top: "40%",
+            transform: "translate(-50%, -50%)",
+          }}
+          class="container position-relative text-center text-lg-center"
+          // data-aos="zoom-out"
+          data-aos-delay="100"
+        >
+          <div class="col-lg-8 font-link">
+            <h1
+              style={{
+                fontFamily: "Rancho",
+                fontSize: "6.5em",
+                marginBottom: "1rem",
+              }}
+            >
+              NutraMeals
+            </h1>
+
+            <h2
+              style={{
+                fontFamily: "Sedgwick Ave Display",
+                fontSize: "2em",
+                marginBottom: "1rem",
+              }}
+            >
+              Your Healthy Food Delivery Partner
+            </h2>
+            <input
+              style={{
+                width: "600px",
+                height: "50px",
+                borderRadius: "50px",
+                textAlign: "center",
+              }}
+              role="combobox"
+              id="mySearch"
+              onkeyup="myFunction()"
+              placeholder="Search for restaurant"
+              title="Type in a category"
+            ></input>
+            <div class="btns">
+              <a href="#menu" class="btn-menu animated fadeInUp scrollto">
+                Menu
+              </a>
+              <a
+                href="#book-a-table"
+                class="btn-book animated fadeInUp scrollto"
+              >
+                Restaurants
+              </a>
             </div>
-          </section>
-        );
-    }
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Home;
-
